400e850e8464b1e651aa3704da9dca4d8fa35dbf
ZIchen XU <zichen_xu@sina.com>
2020-11-08 20:32:20 +0800

    Test($Level): complete JUnit test for Level class
    	- uses debug map for testing suite

--- test/Business/Data/LevelTest.java
+++ test/Business/Data/LevelTest.java
@@ -10,7 +10,8 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 import GameObject.AbstractGameObject;
-import GameObject.Objects.Wall;
+import GameObject.Objects.Crate;
+import GameObject.Objects.IllegalMovementException;
 import org.junit.jupiter.api.Test;
 
 
@@ -20,7 +21,7 @@ class LevelTest {
     }
 
     @Test
-    void testRawLevelParsing() throws Exception {
+    void testRawLevelParsingMap1() throws Exception {
         List<String> level1Raw = LevelTest.lineParser(
                 """
                         WWWWWWWWWWWWW WWWWWW
@@ -28,16 +29,93 @@ class LevelTest {
                         W    W D  C  S     W
                         w    w      WWWWWWWW
                         wwwwwwwwWwwwwwwwwwww""");
-        Field objectsGridField = Level.class.getDeclaredField("objectsGrid");
-        Field diamondsGridField = Level.class.getDeclaredField("diamondsGrid");
         Level testLevel1 = new Level("testLevelName", 1, level1Raw);
+        helperTestGameObjectVerificationOnObjectsGrid(testLevel1, new Point(0, 0), "Wall");
+        helperTestGameObjectVerificationOnObjectsGrid(testLevel1, new Point(0, 19), "Wall");
+        helperTestGameObjectVerificationOnObjectsGrid(testLevel1, new Point(1, 1), "Floor");
+        helperTestGameObjectVerificationOnObjectsGrid(testLevel1, new Point(2, 7), "Floor");
+        helperTestGameObjectVerificationOnObjectsGrid(testLevel1, new Point(2, 10), "Crate");
+        helperTestGameObjectVerificationOnObjectsGrid(testLevel1, new Point(2, 13), "Keeper");
+        for (int i = 0; i < 19; i++) {
+            helperTestGameObjectVerificationOnObjectsGrid(testLevel1, new Point(4, i), "Wall");
+        }
+        helperTestGameObjectVerificationOnDiamondsGrid(testLevel1, new Point(2, 7));
+    }
+
+    @Test
+    void testRawLevelParsingMap2() throws Exception {
+        List<String> level1Raw = LevelTest.lineParser(
+                """
+                        WWWWWWWWWWWWWwWWWWWW
+                        Ws cd              W
+                        wwwwwwwwWwwwwwwwwwww""");
+        Level testLevel1 = new Level("testLevelName", 1, level1Raw);
+        for (int i = 0; i < 19; i++) {
+            helperTestGameObjectVerificationOnObjectsGrid(testLevel1, new Point(0, i), "Wall");
+        }
+        helperTestGameObjectVerificationOnObjectsGrid(testLevel1, new Point(1, 0), "Wall");
+        helperTestGameObjectVerificationOnObjectsGrid(testLevel1, new Point(1, 1), "Keeper");
+        helperTestGameObjectVerificationOnObjectsGrid(testLevel1, new Point(1, 3), "Crate");
+        for (int i = 0; i < 19; i++) {
+            helperTestGameObjectVerificationOnObjectsGrid(testLevel1, new Point(2, i), "Wall");
+        }
+        helperTestGameObjectVerificationOnDiamondsGrid(testLevel1, new Point(1, 4));
+    }
+
+    @Test
+    void testRawLevelParsingMap3() throws Exception {
+        List<String> level1Raw = LevelTest.lineParser(
+                """
+                        WWWWWWWWWWWWW WWWWWW
+                        W    W D  C        W
+                        W    W D  C  S     W
+                        w    w      WWWWWWWW
+                        wwwwwwwwWwwwwwwwwwww""");
+        Level testLevel1 = new Level("testLevelName", 1, level1Raw);
+        helperTestGameObjectVerificationOnObjectsGrid(testLevel1, new Point(0, 13), "Floor");
+        helperTestGameObjectVerificationOnObjectsGrid(testLevel1, new Point(1, 7), "Floor");
+        helperTestGameObjectVerificationOnObjectsGrid(testLevel1, new Point(1, 10), "Crate");
+        helperTestGameObjectVerificationOnObjectsGrid(testLevel1, new Point(2, 13), "Keeper");
+        helperTestGameObjectVerificationOnDiamondsGrid(testLevel1, new Point(1, 7));
+    }
+
+    @Test
+    void testIsCompleteMap1() throws IllegalMovementException {
+        List<String> level1Raw = LevelTest.lineParser(
+                """
+                        WWWWWWWWWWWWW WWWWWW
+                        W    W             W
+                        W    W D  C  S     W
+                        w    w      WWWWWWWW
+                        wwwwwwwwWwwwwwwwwwww""");
+        Level testLevel1 = new Level("testLevelName", 1, level1Raw);
+        ((Crate)testLevel1.objectsGrid.getGameObjectAt(2, 10)).moveBy(new Point(0, -3));
+        assertTrue(testLevel1.isComplete());
+
+    }
+
+
+    private void helperTestGameObjectVerificationOnObjectsGrid(Level testLevel1, Point gameObjectAt, String instanceOf) throws NoSuchFieldException, IllegalAccessException, ClassNotFoundException {
+        Field objectsGridField = Level.class.getDeclaredField("objectsGrid");
         objectsGridField.setAccessible(true);
-        diamondsGridField.setAccessible(true);
         GameGrid objectsGrid = (GameGrid)objectsGridField.get(testLevel1);
-        AbstractGameObject testGameObject1 = objectsGrid.getGameObjectAt(0, 0);
-        assertTrue(testGameObject1 instanceof Wall && testGameObject1.at().equals(new Point(0, 0)));
+        AbstractGameObject testGameObject1 = objectsGrid.getGameObjectAt(gameObjectAt);
+        assertTrue((testGameObject1.getClass() == (Class.forName("GameObject.Objects." + instanceOf))) && testGameObject1.at().equals(gameObjectAt));
         Field objectsGridFieldInGameObject = AbstractGameObject.class.getDeclaredField("grid");
         objectsGridFieldInGameObject.setAccessible(true);
         assertSame((GameGrid)objectsGridFieldInGameObject.get(testGameObject1), objectsGrid);
     }
+
+    private void helperTestGameObjectVerificationOnDiamondsGrid(Level testLevel1, Point gameObjectAt) throws NoSuchFieldException, IllegalAccessException, ClassNotFoundException {
+        Field diamondsGridField = Level.class.getDeclaredField("diamondsGrid");
+        diamondsGridField.setAccessible(true);
+        GameGrid objectsGrid = (GameGrid)diamondsGridField.get(testLevel1);
+        AbstractGameObject testGameObject1 = objectsGrid.getGameObjectAt(gameObjectAt);
+        assertTrue((testGameObject1.getClass() == (Class.forName("GameObject.Objects.Diamond"))) && testGameObject1.at().equals(gameObjectAt));
+        Field objectsGridFieldInGameObject = AbstractGameObject.class.getDeclaredField("grid");
+        objectsGridFieldInGameObject.setAccessible(true);
+        assertSame((GameGrid)objectsGridFieldInGameObject.get(testGameObject1), objectsGrid);
+    }
+
+
 }
